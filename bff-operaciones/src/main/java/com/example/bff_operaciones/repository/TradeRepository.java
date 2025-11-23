95db4e9b7e0c5fd4cb4b067eb60cac6b3f31fb7f
franpvp <franpvp.98@gmail.com>
2025-11-23 12:08:10 -0300

    Se quita antiguo bff

--- bff-operaciones/src/main/java/com/example/bff_operaciones/repository/TradeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.bff_operaciones.repository;
-
-import com.example.bff_operaciones.entity.TradeEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TradeRepository
-        extends JpaRepository<TradeEntity, Long>, TradeRepositoryCustom {
-}
